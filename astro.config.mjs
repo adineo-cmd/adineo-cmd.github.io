5ed2bc23d2c8805e67690649d9a71bbf76e18312
Aditya Yadav <theadityayadav101@gmail.com>
2026-03-08 16:08:53 +0530

    fix sitemap issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,9 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://adineo-cmd.github.io', // ← Must be a full URL with https://
+  site: 'https://adineo-cmd.github.io',
   
-  integrations: [
-    sitemap(),
-  ],
+  output: 'static',
+  
+  integrations: [sitemap()],
 });
