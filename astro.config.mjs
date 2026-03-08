6f2398f4c70707bbb41fa8eec48df834e4079ff9
Aditya Yadav <theadityayadav101@gmail.com>
2026-03-08 14:33:06 +0530

    Add sitmap.xml and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,7 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   integrations: [],
-  site: 'https://adineo-cmd.github.io', 
+  site: 'https://adineo-cmd.github.io',
+  integrations: [sitemap()],
+  output: 'static', 
 });
